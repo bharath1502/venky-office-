65430e7a248af6ea637f405932d3e2c866b7874c
ajith <ajith.murugesan@girmiti.com>
2018-12-05 20:44:27 +0530

    resolved comments

--- chatak-library/src/main/java/com/chatak/pg/util/Constants.java
+++ chatak-library/src/main/java/com/chatak/pg/util/Constants.java
@@ -1008,5 +1008,7 @@ public class Constants {
   public static final String JSESSIONID = "JSESSIONID";
   
   public static final String SETTELEMENT_AMOUNT_FORMAT ="#,##0.00";
+  
+  public static final String ACCOUNT_PAY_VALUE ="350";
 
 }

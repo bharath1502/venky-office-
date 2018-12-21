fe7fb18aea0b43d8c04f6723533a0a3afc4e6041
imtiyaz <imtiyaz@localhost.localdomain>
2018-12-21 19:56:50 +0530

    resolved issue

--- chatak-acquirer-admin/src/main/webapp/js/merchant.js
+++ chatak-acquirer-admin/src/main/webapp/js/merchant.js
@@ -617,7 +617,7 @@ function validateBusinessName() {
 		setError(get('businessName'), webMessages.validationthisfieldismandatory);
 		loadMsgTitleText();
 		return false;
-	} else if (!businessNameRegx.test(businessName)) {
+	} else if (!businessNameRegx.test(businessName)|| !(businessName.length == 2)) {
 		setError(get('businessName'), webMessages.invalidCompanyName);
 		loadMsgTitleText();
 		return false;
@@ -638,7 +638,7 @@ function validateFirstName() {
 		setError(get('firstName'), webMessages.validationthisfieldismandatory);
 		loadMsgTitleText();
 		return false;
-	} else if (!firstNameRegx.test(firstName)) {
+	} else if (!firstNameRegx.test(firstName) || !(firstName.length == 2)) {
 		setError(get('firstName'), webMessages.invalidFirstName);
 		loadMsgTitleText();
 		return false;
@@ -663,7 +663,7 @@ function validateLastName() {
 		setError(get('lastName'), webMessages.validationthisfieldismandatory);
 		loadMsgTitleText();
 		return false;
-	} else if (!lastNameRegx.test(lastName)) {
+	} else if (!lastNameRegx.test(lastName) || !(lastName.length == 2)) {
 		setError(get('lastName'), webMessages.invalidLastName);
 		loadMsgTitleText();
 		return false;

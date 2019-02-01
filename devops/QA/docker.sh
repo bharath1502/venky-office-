13278859a91fd3156b824d11d10821c4c8c8d4c9
Prithvi Prasad <prithvi.prasad@girmiti.com>
2019-02-01 15:59:40 +0530

    docker.sh edited online with Bitbucket

--- devops/QA/docker.sh
+++ devops/QA/docker.sh
@@ -1,7 +1,7 @@
 echo "Pushing the Docker IMAGE"
 #gateway
-docker tag $image:${BUILD_NUMBER} $docker-registery/$image:${BUILD_NUMBER}
-docker push $docker-registery/$image:${BUILD_NUMBER}
+docker tag $image:${BUILD_NUMBER} $docker-registry/$image:${BUILD_NUMBER}
+docker push $docker-registry/$image:${BUILD_NUMBER}
 #paygate
 #docker tag paygate:$version 192.168.2.171:8083/paygate:$version
 #docker push 192.168.2.171:8083/paygate:$version

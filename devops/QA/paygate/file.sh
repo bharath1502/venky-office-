60dc0f169b3157bfbf01b406214af06f9546b3b3
IT Dept <its@girmiti.com>
2019-03-20 19:06:51 +0530

    file.sh edited online with Bitbucket

--- devops/QA/paygate/file.sh
+++ devops/QA/paygate/file.sh
@@ -1,7 +1,7 @@
-admin=$(curl -L $URL/repository/maven-snapshots/$groupid/$artifactid1/4.0.0-SNAPSHOT/maven-metadata.xml | grep -m 1 \<value\> | sed -e 's/<value>\(.*\)<\/value>/\1/' | sed -e 's/ //g')
+chatak-host-pg=$(curl -L $URL/repository/maven-snapshots/$groupid/$artifactid1/4.0.0-SNAPSHOT/maven-metadata.xml | grep -m 1 \<value\> | sed -e 's/<value>\(.*\)<\/value>/\1/' | sed -e 's/ //g')
 wget --no-check-certificate "$URL/repository/maven-snapshots/$groupid/$artifactid1/4.0.0-SNAPSHOT/$artifactid1-${admin}.war" -O $artifactid1.war
 
-merchant=$(curl -L $URL/repository/maven-snapshots/$groupid/$artifactid2/4.0.0-SNAPSHOT/maven-metadata.xml | grep -m 1 \<value\> | sed -e 's/<value>\(.*\)<\/value>/\1/' | sed -e 's/ //g')
+paygate=$(curl -L $URL/repository/maven-snapshots/$groupid/$artifactid2/4.0.0-SNAPSHOT/maven-metadata.xml | grep -m 1 \<value\> | sed -e 's/<value>\(.*\)<\/value>/\1/' | sed -e 's/ //g')
 wget --no-check-certificate "$URL/repository/maven-snapshots/$groupid/$artifactid2/4.0.0-SNAPSHOT/$artifactid2-${merchant}.war" -O $artifactid2.war
 
 #paygate=$(curl -L http://192.168.0.91:8081/repository/maven-snapshots/$groupid/$artifactid3/4.0.0-SNAPSHOT/maven-metadata.xml | grep -m 1 \<value\> | sed -e 's/<value>\(.*\)<\/value>/\1/' | sed -e 's/ //g')
